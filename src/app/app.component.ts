f42f3f8f2dc74d7267c62a2c609c111e3f184567
Eric <ericfr.trabajo@gmail.com>
2026-04-19 12:50:19 +0200

    Editat HTML y CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ElementService } from './services/element.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ioc-angular-adventurerquestboard-eric';
+
+  constructor(public elementService: ElementService) {}
+
+  ngOnInit(){
+    this.elementService.obtenirPopulars();
+  }
 }
